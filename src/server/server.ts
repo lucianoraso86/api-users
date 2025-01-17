786ab8562e6606eda8389c0fefe07d14cc267dc5
lucianoraso86 <lucianoraso86@gmail.com>
2025-01-17 10:10:42 -0300

    - minor fix

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,9 +3,9 @@ import express, { Application } from "express";
 import morgan from "morgan";  
 
 const app: Application = express();
+
 app.use(express.json());
 app.use(morgan("dev"));
-
 app.use("/api/v1", routes());
 
 export default app;
